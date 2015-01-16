4ce2eac3c483219bd1f44eb7a3b27fc45152731e
Yu Kitazume <u.kitazume@gmail.com>
2015-01-16 11:51:58 +0900

    add mkdir

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,1 +1,2 @@
-run "ln -nfs #{config.shared_path}/public #{config.release_path}/public"
+run! "mkdir  #{config.shared_path}/public"
+run! "ln -nfs #{config.shared_path}/public #{config.release_path}/public"
