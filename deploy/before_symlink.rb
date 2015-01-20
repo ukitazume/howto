a79842c0852ae83cbc68846737183a5b52b32998
Yu Kitazume <u.kitazume@gmail.com>
2015-01-20 14:31:10 +0900

    for debug

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,5 @@
 run! "mkdir -p #{config.shared_path}/public"
 run! "ln -s #{config.shared_path}/public #{config.release_path}/public/"
-webroot = config.node['engineyard']['environment']['apps'].select{|app| app.values.include? "git://github.com/engineyard/howto.git"}.first['components'].select{|comp| comp.keys.include? "php_webroot"}.first['php_webroot']
-run! "cat #{webroot} >> /tmp/tmplog"
+# webroot = config.node['engineyard']['environment']['apps'].select{|app| app.values.include? "git://github.com/engineyard/howto.git"}.first['components'].select{|comp| comp.keys.include? "php_webroot"}.first['php_webroot']
+run! "cat #{config.node.to_s} >> /tmp/tmplog"
 
