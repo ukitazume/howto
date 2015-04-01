7adf5b023615073bed7e507641891819d0057f96
Yu Kitazume <u.kitazume@gmail.com>
2015-04-01 10:52:44 +0900

    chagne place

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -3,5 +3,6 @@
 # webroot = config.node[:engineyard][:environment][:apps].select{|app| app.values.include? "git://github.com/ukitazume/howto.git"}.first[:components].select{|comp| comp.keys.include? "php_webroot"}.first['php_webroot']
 # run! "cat \\"#{webroot}\\" >> /tmp/tmplog"
 sudo "eselect nodejs set 0.10.30"
+run "npm install gulp"
 run "gulp"
 
