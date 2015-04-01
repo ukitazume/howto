bbfe17f3874a3a866eeb570bbcb098c81b541198
Yu Kitazume <u.kitazume@gmail.com>
2015-04-01 11:09:34 +0900

    nodejs

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,6 @@
 # -*- coding: euc-jp -*-
-run "export LC_ALL="en_US.utf8"
+run "export LC_ALL=en_US.utf8"
+run "export LC_CTYPE=en_US.UTF-8"
 sudo "eselect nodejs set 0.10.30"
 run "npm install gulp"
 run "gulp"
