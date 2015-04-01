a7e474e525f1295139dec78c84bef582589391a4
Yu Kitazume <u.kitazume@gmail.com>
2015-04-01 11:07:06 +0900

    nodejs

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,8 +1,5 @@
 # -*- coding: euc-jp -*-
-# run! "mkdir -p #{config.shared_path}/public"
-# run! "ln -s #{config.shared_path}/public #{config.release_path}/public/"
-# webroot = config.node[:engineyard][:environment][:apps].select{|app| app.values.include? "git://github.com/ukitazume/howto.git"}.first[:components].select{|comp| comp.keys.include? "php_webroot"}.first['php_webroot']
-# run! "cat \\"#{webroot}\\" >> /tmp/tmplog"
+run "export LC_ALL="en_US.utf8"
 sudo "eselect nodejs set 0.10.30"
 run "npm install gulp"
 run "gulp"
