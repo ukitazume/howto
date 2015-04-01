a0c0aab6f807a78d13615c03565069d628cbb21d
Yu Kitazume <u.kitazume@gmail.com>
2015-04-01 11:01:50 +0900

    nodejs

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,4 @@
+# -*- coding: euc-jp -*-
 # run! "mkdir -p #{config.shared_path}/public"
 # run! "ln -s #{config.shared_path}/public #{config.release_path}/public/"
 # webroot = config.node[:engineyard][:environment][:apps].select{|app| app.values.include? "git://github.com/ukitazume/howto.git"}.first[:components].select{|comp| comp.keys.include? "php_webroot"}.first['php_webroot']
